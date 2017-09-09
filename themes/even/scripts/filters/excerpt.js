6e6ab1e5224eca47ea155bae87b6574715354df0
huanz <260540233@qq.com>
2017-09-10 02:17:17 +0800

    change theme

---
+++ themes/even/scripts/filters/excerpt.js
@@ -0,0 +1,12 @@
+var rExcerpt = /<p>&lt;!--\s*more\s*--&gt;<\/p>/i;
+hexo.extend.filter.register('after_post_render', function (data) {
+    var content = data.content;    
+    if (!data.excerpt && rExcerpt.test(content)) {
+        data.content = content.replace(rExcerpt, function (match, index) {
+            data.excerpt = content.substring(0, index).trim();
+            data.more = content.substring(index + match.length).trim();
+
+            return '<a id="more"></a>';
+        });
+    }
+});
